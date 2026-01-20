296800a2337347303b3aef1c9c6385dbee34424c
Mário Costa <mariocostaa6@gmail.com>
2026-01-20 03:47:39 +0000

    feat: installed the TypeORM, and edit the app.module, to include it.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthController } from './auth/auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    autoLoadEntities: true,
+    synchronize: true,
+  })],
+  controllers: [AppController, AuthController],
   providers: [AppService],
 })
 export class AppModule {}
